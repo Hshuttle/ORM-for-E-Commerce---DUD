1ca123ede9fedda30582c1ac540683de139b4476
Hshuttle <huntershutle@gmail.com>
2022-11-09 21:29:11 -0500

    started index.js, added in my require statements and exports

---
+++ Models/index.js
@@ -0,0 +1,6 @@
+const Category = require("./category");
+const Product = require("./product");
+const Tag = require("./tag");
+const ProductTag = require("./producttag");
+
+module.exports = { Category, Product, Tag, ProductTag };
